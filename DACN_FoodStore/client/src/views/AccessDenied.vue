6a785fb7dd7f63285b55b2fac4c8df0e9a1b4317
nguyenthuytrucqh2003 <nguyenthuytrucqh20032gmail.com>
2025-01-04 17:33:39 +0700

    change thuytruc branch

--- DACN_FoodStore/client/src/views/AccessDenied.vue
+++ DACN_FoodStore/client/src/views/AccessDenied.vue
@@ -5,8 +5,8 @@
     <div class="container">
         <img src="../assets/access-denied.png" alt="">
         <h4>Không thể truy cập vào trang !</h4>
-    
-        <h4>Thuy Truc comment !</h4>
+     <h4>thuy truc conflict</h4>
+        <h4>Thuy Truc comment conflict!</h4>
     </div>
 </template>
 <style scoped>
